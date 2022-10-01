a5b6ef22f69d4c4c039e24412cf4b2ff6d38dc87
Davraph <rapheal98@icloud.com>
2022-10-01 06:56:21 +0200

    Added test for listing users and Make Django admin list users.

--- app/core/admin.py
+++ app/core/admin.py
@@ -7,9 +7,11 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from core import models
 
+
 class UserAdmin(BaseUserAdmin):
     """ Define the admin pages for users. """
     ordering = ['id']
     list_display = ['email', 'name', ]
+
 
 admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -7,6 +7,7 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import Client
 
+
 class AdminSiteTests(TestCase):
     """ Tests for Django admin ."""
     def setUp(self):
@@ -22,7 +23,6 @@ class AdminSiteTests(TestCase):
             password='testpass123',
             name='Test User'
         )
-
 
     def test_users_list(self):
         """ Test that users are listed on page."""
