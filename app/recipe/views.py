a501d29060763b392140d62929dfa40d7b0d9935
Davraph <rapheal98@icloud.com>
2022-10-05 02:17:06 +0200

    Add create recipe API and tests

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -26,3 +26,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return serializers.RecipeSerializer
 
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        """ Create a new reciipe """
+        serializer.save(user=self.request.user)
