985aecd52b2a4da2cc0f1b41f7ef65eb3063b1f6
Davraph <rapheal98@icloud.com>
2022-10-10 10:23:04 +0200

    Added test for ingredient listing api

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -13,7 +13,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import  Recipe, Tag
+from core.models import Recipe, Tag
 
 
 from recipe.serializers import (
